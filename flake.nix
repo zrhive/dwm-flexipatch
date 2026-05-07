2bcbe00098a4aa0cead9452d3faf72e1a4b18955
zhyie <128297362+zhyie@users.noreply.github.com>
2026-05-08 04:59:04 +0800

    feat: flake package

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "dwm-flexipatch";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs }:
+    let
+      lib = nixpkgs.lib;
+      systems = lib.systems.flakeExposed;
+      eachSystem = function: lib.genAttrs systems (system: function (pkgs system));
+      pkgs = system: nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages = eachSystem (pkgs: {
+        dwm-flexipatch = pkgs.dwm.overrideAttrs (old: {
+          src = ./dwm;
+          buildInputs =
+            old.buildInputs
+            ++ builtins.attrsValues {
+              inherit (pkgs)
+                libxcursor
+                imlib2
+                ;
+            };
+        });
+      });
+    };
+}
